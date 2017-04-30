14ccd51153007ddf74711e3ae60c8c02d34d0c1a
Max Spliethöver <m.spliethoever@gmail.com>
2017-04-30 19:09:46 +0200

    Add option to print with standard paramters

--- e0-lpr.sh
+++ e0-lpr.sh
@@ -22,33 +22,26 @@ fi
 
 ssh "${HOST}" -- pacc
 
+STANDARD_PARAMETERS=false
 PRINTER=''
-PS3='Select printer: '
-select OPT in 'lw0' 'lw1'; do 
-  case "${REPLY}" in
-    1|2 )
-      PRINTER="${OPT}"
-      break
-      ;;
-
-    *)
-      echo 'Please answer with '"'"'1'"'"' or '"'"'2'"'"'.'
-      continue
-      ;;
-  esac
-done
-
 SIZE=''
-PS3='Select paper size: '
-select OPT in 'DIN A4' 'DIN A3'; do 
+DUPLEX=''
+COPIES=0
+
+PS3='Use standard parameters (lw0, DIN A4, two-sided-long-edge, 1 copy)? '
+select OPT in 'Yes' 'No'; do
   case "${REPLY}" in
     1 )
-      SIZE="A4"
+      PRINTER='lw0'
+      SIZE='A4'
+      DUPLEX='two-sided-long-edge'
+      COPIES=1
+      STANDARD_PARAMETERS=true
       break
       ;;
 
     2 )
-      SIZE='A3'
+      STANDARD_PARAMETERS=false
       break
       ;;
 
@@ -59,30 +52,60 @@ select OPT in 'DIN A4' 'DIN A3'; do
   esac
 done
 
-DUPLEX=''
-PS3='Select duplex options: '
-select OPT in 'one-sided' 'two-sided-long-edge' 'two-sided-short-edge'; do 
-  case "${REPLY}" in
-    1|2|3 )
-      DUPLEX="${OPT}"
-      break
-      ;;
+if [[ "${STANDARD_PARAMETERS}" = false ]]; then
+  PS3='Select printer: '
+  select OPT in 'lw0' 'lw1'; do 
+    case "${REPLY}" in
+      1|2 )
+        PRINTER="${OPT}"
+        break
+        ;;
 
-    *)
-      echo 'Please answer with '"'"'1'"'"', '"'"'2'"'"' or '"'"'3'"'"'.'
-      continue
-      ;;
-  esac
-done
+      *)
+        echo 'Please answer with '"'"'1'"'"' or '"'"'2'"'"'.'
+        continue
+        ;;
+    esac
+  done
 
-COPIES=0
-while [ ! "${COPIES}" -gt 0 ]; do
-  printf 'Number of copies: '
-  read -r COPIES
-  if ! [[ "${COPIES}" =~ ^[0-9]+$ ]]; then
-    COPIES=0
-  fi
-done
+  PS3='Select paper size: '
+  select OPT in 'DIN A4' 'DIN A3'; do 
+    case "${REPLY}" in
+      1|2 )
+        SIZE="${OPT}"
+        break
+        ;;
+
+      *)
+        echo 'Please answer with '"'"'1'"'"' or '"'"'2'"'"'.'
+        continue
+        ;;
+    esac
+  done
+
+  PS3='Select duplex options: '
+  select OPT in 'one-sided' 'two-sided-long-edge' 'two-sided-short-edge'; do 
+    case "${REPLY}" in
+      1|2|3 )
+        DUPLEX="${OPT}"
+        break
+        ;;
+
+      *)
+        echo 'Please answer with '"'"'1'"'"', '"'"'2'"'"' or '"'"'3'"'"'.'
+        continue
+        ;;
+    esac
+  done
+
+  while [ ! "${COPIES}" -gt 0 ]; do
+    printf 'Number of copies: '
+    read -r COPIES
+    if ! [[ "${COPIES}" =~ ^[0-9]+$ ]]; then
+      COPIES=0
+    fi
+  done
+fi
 
 TMP="$( ssh ${HOST} -- mktemp --tmpdir=\"$\( pwd \)\" e0-lpr.sh.tmp.XXXXX )"
 PARAMETERS="-P ${PRINTER} -# ${COPIES} -o sides=${DUPLEX} -o media=${SIZE}"
