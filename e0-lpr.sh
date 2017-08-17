ac922d249151c7db79d07a46760d9f12385faa19
Michel Zimmer <mzimmer@uni-bremen.de>
2017-08-17 19:51:23 +0200

    Add Travis CI configuration

--- e0-lpr.sh
+++ e0-lpr.sh
@@ -9,15 +9,15 @@
 HOST='x01.informatik.uni-bremen.de'
 
 if [ "${#}" -ne 1 ]; then
-  echo "Usage: ${0} filename"
-  exit 0
+    echo "Usage: ${0} filename"
+    exit 0
 fi
 
 FILENAME="${1}"
 
 if [ ! -f "${FILENAME}" ]; then
-  echo "File not found: ${FILENAME}"
-  exit 0
+    echo "File not found: ${FILENAME}"
+    exit 0
 fi
 
 ssh "${HOST}" -- pacc
@@ -30,81 +30,81 @@ COPIES=0
 
 PS3='Use standard parameters (lw0, DIN A4, two-sided-long-edge, 1 copy)? '
 select OPT in 'Yes' 'No'; do
-  case "${REPLY}" in
-    1 )
-      PRINTER='lw0'
-      SIZE='A4'
-      DUPLEX='two-sided-long-edge'
-      COPIES=1
-      STANDARD_PARAMETERS=true
-      break
-      ;;
+    case "${REPLY}" in
+        1 )
+            PRINTER='lw0'
+            SIZE='A4'
+            DUPLEX='two-sided-long-edge'
+            COPIES=1
+            STANDARD_PARAMETERS=true
+            break
+            ;;
 
-    2 )
-      STANDARD_PARAMETERS=false
-      break
-      ;;
+        2 )
+            STANDARD_PARAMETERS=false
+            break
+            ;;
 
-    *)
-      echo 'Please answer with '"'"'1'"'"' or '"'"'2'"'"'.'
-      continue
-      ;;
-  esac
+        *)
+            echo 'Please answer with "1" or "2".'
+            continue
+            ;;
+    esac
 done
 
 if [[ "${STANDARD_PARAMETERS}" = false ]]; then
-  PS3='Select printer: '
-  select OPT in 'lw0' 'lw1'; do 
-    case "${REPLY}" in
-      1|2 )
-        PRINTER="${OPT}"
-        break
-        ;;
+    PS3='Select printer: '
+    select OPT in 'lw0' 'lw1'; do
+        case "${REPLY}" in
+            1|2 )
+                PRINTER="${OPT}"
+                break
+                ;;
 
-      *)
-        echo 'Please answer with '"'"'1'"'"' or '"'"'2'"'"'.'
-        continue
-        ;;
-    esac
-  done
+            *)
+                echo 'Please answer with "1" or "2".'
+                continue
+                ;;
+        esac
+    done
 
-  PS3='Select paper size: '
-  select OPT in 'DIN A4' 'DIN A3'; do 
-    case "${REPLY}" in
-      1|2 )
-        SIZE="${OPT}"
-        break
-        ;;
+    PS3='Select paper size: '
+    select OPT in 'DIN A4' 'DIN A3'; do
+        case "${REPLY}" in
+            1|2 )
+                SIZE="${OPT}"
+                break
+                ;;
 
-      *)
-        echo 'Please answer with '"'"'1'"'"' or '"'"'2'"'"'.'
-        continue
-        ;;
-    esac
-  done
+            *)
+                echo 'Please answer with "1" or "2".'
+                continue
+                ;;
+        esac
+    done
 
-  PS3='Select duplex options: '
-  select OPT in 'one-sided' 'two-sided-long-edge' 'two-sided-short-edge'; do 
-    case "${REPLY}" in
-      1|2|3 )
-        DUPLEX="${OPT}"
-        break
-        ;;
+    PS3='Select duplex options: '
+    select OPT in 'one-sided' 'two-sided-long-edge' 'two-sided-short-edge'; do
+        case "${REPLY}" in
+            1|2|3 )
+                DUPLEX="${OPT}"
+                break
+                ;;
 
-      *)
-        echo 'Please answer with '"'"'1'"'"', '"'"'2'"'"' or '"'"'3'"'"'.'
-        continue
-        ;;
-    esac
-  done
+            *)
+                echo 'Please answer with "1", "2" or "3".'
+                continue
+                ;;
+        esac
+    done
 
-  while [ ! "${COPIES}" -gt 0 ]; do
-    printf 'Number of copies: '
-    read -r COPIES
-    if ! [[ "${COPIES}" =~ ^[0-9]+$ ]]; then
-      COPIES=0
-    fi
-  done
+    while [ ! "${COPIES}" -gt 0 ]; do
+        printf 'Number of copies: '
+        read -r COPIES
+        if ! [[ "${COPIES}" =~ ^[0-9]+$ ]]; then
+            COPIES=0
+        fi
+    done
 fi
 
 TMP="$( ssh ${HOST} -- mktemp --tmpdir=\"$\( pwd \)\" e0-lpr.sh.tmp.XXXXX )"
@@ -114,15 +114,17 @@ echo 'Print using the following parameters? [Y/n]'
 echo "${PARAMETERS}"
 read -r CONTINUE
 case "${CONTINUE}" in
-  [yY])
-    ;;
-  '')
-    ;;
-  *)
-    exit 0
-    ;;
+    [yY])
+        ;;
+    '')
+        ;;
+    *)
+        exit 0
+        ;;
 esac
 
 scp "${FILENAME}" "${HOST}:${TMP}"
+# shellcheck disable=SC2029
 ssh "${HOST}" -- "lpr ${PARAMETERS} '${TMP}'"
+# shellcheck disable=SC2029
 ssh "${HOST}" -- "rm '${TMP}'"
